fbdf1d1f142deab9eba57af86ba2e563377cdb69
David Deloo <Deloo@iMac-de-David.local>
2015-05-19 18:23:55 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,115 @@
+Nimail | Responsive Email Starterkit
+====================
+
+**Nimail** is a starterkit base on the [unofficial Sass port of Ink](https://github.com/faustgertz/sassy-ink) of Zurb's excellent [Ink](http://zurb.com/ink/) responsive email template system. Nimail is integrated into a Gulp and Sass workflow for automated Css inlining, editing template with includes partials, uploads assets and easy email testing with [Mailgun](http://www.mailgun.com/).
+
+
+#### Features :
+- BrowserSync livereload development
+- Zurb’s Ink template system
+- Sass compilation
+- Templates compilation using partials includes
+- CSS inlining
+- FTP uploads
+- Sending test email using Mailgun
+
+
+## Installation
+
+#### Download this repository as a ZIP file or clone it to your local machine :
+
+	git clone git@github.com:hellodeloo/nimail.git
+
+
+#### You'll also need the following tools :
+
+- [Node.js](http://nodejs.org/)
+- [Gulp](http://gulpjs.com/)
+- [Sass](http://sass-lang.com/)
+- [Mailgun](http://www.mailgun.com/) *[API Key]*
+
+
+#### At the root of your local repository :
+
+1. Duplicate the directory `/nimail-sassy-ink/`  (or nimail-saucy-ink *) and give it the name of your new email
+2. Adjust some **vars** and **paths** in the `gulpfile.js` file
+3. Add your partial files in the `/partials/` directory
+4. Work your html structure in the **index.temp.html** file
+5. Add your specific styles in the `/stylesheets/scss/styles.scss` file
+
+#### In the directory structure :
+- `index.temp.html` is your main html working file
+- `index.html` is the html file that is launched in the livereload server
+- `mail.html` is the delivery file with inline styles
+- `test.html` is the test sending file with inline styles and absolute images links
+
+```
+├── your-new-nimail-sassy-ink/
+│   ├── index.html
+│   ├── index.temp.html
+│   ├── mail.html
+│   ├── test.html
+│   ├── partials/
+│   │   ├── *_.part.html
+│   ├── stylesheets/
+│   │   ├── css
+│   │   ├── scss
+```
+
+
+
+## Usage
+
+#### First, adjust some vars and paths in the in the `gulpfile.js` file :
+- The name of the working directory of your email
+- The absolute url where the email images from the test will be accessible
+- The FTP directory where the email will be transferred files
+- Host of your FTP
+- Login of your FTP
+- Password of your FTP
+- The API Key of your Mailgun account
+- The email address of the sender of the email test
+- The email address of the email recipient test (can be the email address of your [Litmus](http://www.litmus.com/) ou [Email on acid](http://www.emailonacid.com/) account)
+- The subject of your email test
+
+
+#### The `gulpfile.js` contains some useful tasks :
+###### Development with `gulp serve`
+
+To compile your assets during development, run `gulp serve` in the command line.  This will do the following :
+
+- Run the BrowserSync livereload development server, it will refresh your browser window with any changes
+- Compile the Sass files within the `/sass/` folder and render them in the `/css/` folder
+- Compile all partials in the `index.html` file and launch it in the server
+
+
+##### Deployment with `gulp build`
+
+When you're ready to « build » your changes and show them to your email marketing service, run `gulp build` to do everything the standard command does, with it more:
+
+- Inline the CSS styles in the `mail.html` and `test.html` files
+- Insert the media queries styles in the document <head> of the `mail.html` and `test.html` files (so the media queries don't get stripped)
+- Replace all images relative links with absolute links in the `test.html` file
+- Uploads the files on the ftp folder you use
+
+
+##### Email Sending with `gulp send`
+
+We use [Mailgun](http://www.mailgun.com/) service to send a test email with your template via the  `gulp send` command line.
+You need to register on mailgun to have an account and a your API Key number.
+
+
+## Notes
+*nimail-saucy-ink is my modified [sassy-ink](https://github.com/faustgertz/sassy-ink) containing some ajustememnet as :
+- Changing the horizontal gutter widths applied to the grids
+- Added support for the visibility classes on Gmail
+- Adding a fluid block-grid
+- Adding a tiny-only medias-queries
+You should find them by searching  /* Saucy Ink custom */ in the scss files (yes i know, i must use a additionnal scss file to put my ajustememnet)
+
+You should see [Email on acid](http://www.emailonacid.com/) tests for **Sassy-nimail-ink** [here](https://www.emailonacid.com/app/acidtest/display/summary/Ia7wGO09xB8rsk1u6J8KsxqpQw0Of2s6bf9i2SagmBy1i/shared) and for **Saucy-nimail-ink** [here](https://www.emailonacid.com/app/acidtest/display/summary/vntH4QaOytAOSSMrQGQY3DCxuZkdIjOEfzUdzjRPqN4dS/shared).
+
+
+## Inspired By…
+- [Generator Gulp Ink Email](https://github.com/lightingbeetle/generator-gulp-ink-email)
+- [Responsive Email Kit](https://github.com/itsahappymedium/responsive-email-kit)
---
+++ gulpfile.js
@@ -0,0 +1,177 @@
+/* =====================================================
+
+Nimail Starterkit Gulpfile
+
+======================================================
+
+The settings you need to customize
+
+1.  The name of the working directory of your email
+2.  The absolute url where the email images from the test will be accessible
+3.  The FTP directory where the email will be transferred files
+4.  Host of your FTP
+5.  Login of your FTP
+6.  Password of your FTP
+7.  The API Key of your Mailgun account
+8.  The email address of the sender of the email test
+9.  The email address of the email recipient test (can be the email address of your [Litmus](http://www.litmus.com/) ou [Email on acid](http://www.emailonacid.com/) account)
+10. The subject of your email test
+
+====================================================== */
+
+var config = {
+    nimaillocaldir: 'nimail-sassy-ink/', /*1*/
+    nimailtesturl: 'http://www.your-domain.com/client-name/your-new-nimail-sassy-ink/', /*2*/
+    nimailftpdir: 'www/client-name/your-new-nimail-sassy-ink/', /*3*/
+    nimailftphost: 'host', /*4*/
+    nimailftplogin: 'login', /*5*/
+    nimailftppass: 'pwd', /*6*/
+    nimailkey: 'key-mailgun', /*7*/
+    nimailsender: 'you@your-domain.com', /*8*/
+    nimailrecipient: 'test email address', /*9*/
+    nimailsubject: 'Email test: your-new-nimail-sassy-ink' /*10*/
+};
+
+
+
+
+/* =====================================================
+
+    Below, change only if you know
+
+====================================================== */
+
+// Gulp Requires
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    replace = require('gulp-replace'),
+    ftp = require('gulp-ftp'),
+    sendmail = require('gulp-mailgun');
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    fileinclude = require('gulp-file-include'),
+    inlineSource = require('gulp-inline-source'),
+    inlineCss = require('gulp-inline-css'),
+    runSequence = require('run-sequence');
+
+
+// Default Paths
+var paths = {
+    nimailtemp: config.nimaillocaldir+'*.temp.html',
+    nimailpartials: config.nimaillocaldir+'partials/*.part.html',
+    nimailindex: config.nimaillocaldir+'index.html',
+    nimailtest: config.nimaillocaldir+'test.html',
+    nimailstylesheets: [config.nimaillocaldir+'stylesheets/scss/**/*.scss', config.nimaillocaldir+'stylesheets/scss/*.scss']
+};
+
+
+// Convert Stylesheets Task
+gulp.task('convertstyles', function() {
+    return gulp.src(paths.nimailstylesheets)
+    .pipe(plumber({
+        errorHandler: function(err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sass())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.nimaillocaldir+'stylesheets/css'));
+});
+
+
+// Partials include Task
+gulp.task('fileinclude', function() {
+  gulp.src(paths.nimailtemp)
+    .pipe(fileinclude({
+      prefix: '@',
+      basepath: '@file'
+    }))
+    .pipe(rename({extname: ''}))
+    .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest(config.nimaillocaldir));
+});
+
+
+// Inline Stylesheets Task
+gulp.task('inlinestyles', function() {
+  return gulp.src(paths.nimailindex)
+    .pipe(inlineSource())
+    .pipe(inlineCss({
+      preserveMediaQueries: true,
+      applyWidthAttributes: true
+    }))
+    .pipe(rename('mail.html'))
+    .pipe(gulp.dest(config.nimaillocaldir));
+});
+
+
+// BrowserSync Task
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: config.nimaillocaldir
+        }
+    });
+    gulp.watch(paths.nimailstylesheets, ['convertstyles', reload]);
+    gulp.watch([paths.nimailtemp, paths.nimailpartials], ['fileinclude', reload]);
+    gulp.watch(paths.nimailindex, ['inlinestyles', 'replace', reload]);
+});
+
+
+// Replace Task
+gulp.task('replace', function(){
+  return gulp.src(config.nimaillocaldir+'mail.html')
+    .pipe(replace('img src="', 'img src="'+config.nimailtesturl))
+    .pipe(rename('test.html'))
+    .pipe(gulp.dest(config.nimaillocaldir));
+})
+
+
+// Upload Task
+gulp.task('upload', function () {
+    return gulp.src(config.nimaillocaldir+'**/*')
+    .pipe(ftp({
+        host: config.nimailftphost,
+        user: config.nimailftplogin,
+        pass: config.nimailftppass,
+        remotePath: config.nimailftpdir
+    }));
+});
+
+
+// Sendmail Task
+gulp.task('sendmail', function () {
+    gulp.src(config.nimaillocaldir+'test.html')
+    .pipe(sendmail({
+        key: config.nimailkey,
+        sender: config.nimailsender,
+        recipient: config.nimailrecipient,
+        subject: config.nimailsubject
+    }));
+});
+
+
+// Default Tasks
+gulp.task('default', function() {
+    runSequence('convertstyles', 'fileinclude');
+});
+
+// Serve Tasks
+gulp.task('serve', function() {
+    runSequence('convertstyles', 'fileinclude', 'serve');
+});
+
+// Build Tasks
+gulp.task('build', function() {
+    runSequence('convertstyles', 'fileinclude', 'inlinestyles', 'replace', 'upload');
+});
+
+// Sendmail Tasks
+gulp.task('send', function() {
+    runSequence('sendmail');
+});
